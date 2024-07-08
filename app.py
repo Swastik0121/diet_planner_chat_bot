d458c3c33381928da4bdab786ce0215b52e8de46
Swastik0121 <125267637+Swastik0121@users.noreply.github.com>
2024-07-08 11:21:37 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,151 @@
+import streamlit as st
+import mysql.connector
+import os
+from groq import Groq
+import random
+
+from langchain.chains import ConversationChain, LLMChain
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain_core.messages import SystemMessage
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+
+def create_connection():
+    conn = None
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',     
+            database='chat_history', 
+            user='root',     
+            password=''  
+        )
+        if conn.is_connected():
+            print('Connected to MySQL database')
+        return conn
+    except Error as e:
+        print(f"Error: {e}")
+    return conn
+
+def create_table(conn):
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS chat_history (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_question TEXT NOT NULL,
+        ai_response TEXT NOT NULL
+    );
+    """
+    try:
+        cursor = conn.cursor()
+        cursor.execute(create_table_sql)
+        conn.commit()
+    except Error as e:
+        print(f" Creating Table Error: {e}")
+
+def insert_chat_history(conn, user_question, ai_response):
+    sql = ''' INSERT INTO chat_history (user_question, ai_response)
+              VALUES (%s, %s) '''
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql, (user_question, ai_response))
+        conn.commit()
+    except Error as e:
+        print(f"Insertion Error: {e}")
+
+def main():
+    
+    # Get Groq API key
+    groq_api_key = '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'
+
+    # Display the Groq logo
+    spacer, col = st.columns([5, 1])  
+    with col:  
+        st.markdown("**Nutri :green[Guide]**")
+
+    # The title and greeting message of the Streamlit application
+    st.title("Chat with NutriGuide!")
+    st.write("Hello! I'm your Nutrition Guide. Feel free to tell me about your requirements!!")
+
+    ##############################
+    system_prompt = ''' You are a diet planner chatbot. Your goal is to to recommend 7 breakfast names, 7 lunch names, 7 dinner names and 5 workout names, while ensuring that the person's cuisine choice and his/her allergies are strictly taken into consideration avoiding ingredients which give rise to the allergy along with his or her goal of the diet, also mention the macros and calories for each recommendation based on the series of questions that you will ask. After collecting the responses, you will provide a personalized plan.
+    '''
+    model = 'mixtral-8x7b-32768'
+    conversational_memory_length = 12
+
+    memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
+
+    user_question = st.text_input("Create your personalized diet plan by letting us know about yourself!!")
+
+    # session state variable
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history=[]
+    else:
+        for message in st.session_state.chat_history:
+            memory.save_context(
+                {'input':message['human']},
+                {'output':message['AI']}
+                )
+
+    # Initialize Groq Langchain chat object and conversation
+    groq_chat = ChatGroq(
+            groq_api_key=groq_api_key, 
+            model_name=model
+    )
+
+    # Create a connection to the MySQL database
+    conn = create_connection()
+
+    # Create the chat_history table
+    if conn is not None:
+        create_table(conn)
+
+    # If the user has asked a question,
+    if user_question:
+
+        # Construct a chat prompt template using various components
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(
+                    content=system_prompt
+                ),  # This is the persistent system prompt that is always included at the start of the chat.
+
+                MessagesPlaceholder(
+                    variable_name="chat_history"
+                ),  # This placeholder will be replaced by the actual chat history during the conversation. It helps in maintaining context.
+
+                HumanMessagePromptTemplate.from_template(
+                    "{human_input}"
+                ),  # This template is where the user's current input will be injected into the prompt.
+            ]
+        )
+
+        # Create a conversation chain using the LangChain LLM (Language Learning Model)
+        conversation = LLMChain(
+            llm=groq_chat,  # The Groq LangChain chat object initialized earlier.
+            prompt=prompt,  # The constructed prompt template.
+            verbose=True,   # Enables verbose output, which can be useful for debugging.
+            memory=memory,  # The conversational memory object that stores and manages the conversation history.
+        )
+        
+        # The chatbot's answer is generated by sending the full prompt to the Groq API.
+        response = conversation.predict(human_input=user_question)
+        message = {'human':user_question,'AI':response}
+        st.session_state.chat_history.append(message)
+        
+        # Store the chat history in the MySQL database
+        if conn is not None:
+            insert_chat_history(conn, user_question, response)
+
+        st.write("NutriGuide:", response)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
